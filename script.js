bd14857f84c5286b72e63ae55b8cfc7ebc2af14b
hudsonlpp <hudsonluizperes@poli.ufrj.br>
2022-10-17 17:26:58 -0300

    projeto finalizado com bônus 1 e bug do preço total resolvido

--- script.js
+++ script.js
@@ -63,13 +63,20 @@ function enviarMensagem(zap){
     const numeroP = Number(compras.valorP.replace("R$ ","").replace (",","."));
     const numeroB = Number(compras.valorB.replace("R$ ","").replace (",","."));
     const numeroS = Number(compras.valorS.replace("R$ ","").replace (",","."));
+    alert(numeroP)
     const total = (numeroP+numeroB+numeroS).toFixed(2).replace (".",",");
+
+    const cliente = prompt("informe por favor o seu nome")
+    const endereco = prompt("informe por favor o seu endereço")
 
     const mensagem =  `Olá, gostaria de fazer o pedido:
     - Prato: ${compras.nomeP}
     - Bebida: ${compras.nomeB}
     - Sobremesa: ${compras.nomeS}
-    Total: R$ ${total}`;
+    Total: R$ ${total}
+    
+    Nome: ${cliente}
+    Endereço: ${endereco}`;
     const encoded = encodeURIComponent(mensagem);
     window.open("https://wa.me/+5524992977985?text=" + encoded, '_blank');
 }
